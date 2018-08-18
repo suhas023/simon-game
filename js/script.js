f03013e23cb8753070d863fef5f8e9a2a0ab6073
suhas023 <notsuhas@gmail.com>
2018-08-18 16:32:41 +0530

    added audio and buttons to start and toggle strict mode

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ class Colors {
 		this.active = false;
 		this.colorElems = document.getElementsByClassName("color");
 		for(let colorElem of this.colorElems)
-			colorElem.addEventListener("click", this.colorClick.bind(this));
+			colorElem.addEventListener("click", this.colorElemClick.bind(this));
 	}
 
 	activate() {
@@ -26,23 +26,30 @@ class Colors {
 			colorElem.style.cursor = "default";
 	}
 
-	colorClick(event) {
+	isActive() {
+		return this.active;
+	}
+	colorElemClick(event) {
 		let target = event.target;
 		if(this.active && target.classList.contains("color")) {
-			this.colorBlink(target);
+			this.colorElemBlink(target);
 		}
 	}
 
-	colorBlink(colorElem) {
+	colorElemBlink(colorElem) {
 		let color = colorElem.classList[0];
-		colorElem.classList.replace(color, color + '-light');
-		colorAudio[color].play();
-		setTimeout(()=>colorElem.classList.replace(color + '-light', color), 300);
+		if(colorAudio[color]) {
+			colorElem.classList.replace(color, color + '-light');
+			colorAudio[color].play();
+			setTimeout(() => colorElem.classList.replace(color + '-light', color), 300);
+		}
+		else
+			alert("error occored");
 	}
 
 	getColorElem(color) {
 		for(let colorElem of this.colorElems)
-			if (colorElem.classList.contains(color))
+			if (colorElem.classList.contains(color) || colorElem.classList.contains(color + '-light'))
 				return colorElem;
 	}
 
@@ -56,11 +63,14 @@ class Simon extends Colors{
 	constructor() {
 		super();
 		this.colorSequence = [];
+		this.userSequence = [];
 		this.counter = 0;
+		this.playerActivate = false;
+		this.strict = false;
 		this.counterView = document.getElementsByClassName('count')[0];
 		this.counterView.textContent = this.counter;
-
-		this.strict = false;
+		document.getElementsByClassName("start")[0].addEventListener("click", () => this.start());
+		document.getElementsByClassName("strict")[0].addEventListener("click", (event) => this.toggleStrict(event));
 	}
 
 	incrementCounter() {
@@ -68,9 +78,85 @@ class Simon extends Colors{
 		this.counterView.textContent = this.counter;
 	}
 
+	extendColorSequence() {
+		this.colorSequence.push(this.getRandomColor());
+	}
+
+	playColors() {
+		let waitTime = 0;
+		for(let color of this.colorSequence) {
+			let colorElem = this.getColorElem(color);
+			setTimeout(() => this.colorElemBlink(colorElem), waitTime * 1000 + 1000);
+			waitTime++;
+		}
+		this.playerActivate = true;
+		setTimeout(()=>this.changeTurn(false),waitTime * 1000 + 1000);
+	}
+
+	start() {
+		this.colorSequence = [];
+		this.userSequence = [];
+		this.counter = 0;
+		this.counterView.textContent = this.counter;
+		this.playerActivate = false;
+		this.changeTurn(true);
+	}
+
+	changeTurn(appendColor) {
+		if(!this.playerActivate){
+			if(appendColor)
+				this.extendColorSequence();
+			this.playColors();
+		}
+		else {
+			this.userSequence = [];
+			this.playerTurn();
+		}
+	}
+
+	playerTurn() {
+		this.activate();
+	}
+
+	colorElemClick(event) {
+		let color;
+		let matched;
+		let target = event.target;
+		if(this.isActive()) {
+			color = target.classList[0];
+			this.deactivate();
+			this.playerActivate = false;
+			this.colorElemBlink(target);
+			this.userSequence.push(color);
+			matched = this.match();
+			if(matched && (this.userSequence.length === this.colorSequence.length)){
+				this.incrementCounter();
+				setTimeout(()=>this.changeTurn(true), 500);
+			}
+			else if(matched)
+				setTimeout(()=>this.playerTurn(), 500);
+			else {
+				if(this.strict)
+					this.start();
+				else
+					this.changeTurn(false);
+			}
+		}
+	}
+
+	match() {
+		return this.userSequence.every((color, index) =>color.includes(this.colorSequence[index]));
+	}
+
+	toggleStrict(event) {
+		this.strict = !this.strict;
+		if(this.strict)
+			event.target.classList.replace("off", "on")
+		else
+			event.target.classList.replace("on", "off")
+	}
 }
 
 x = new Simon();
-x.activate();
 
-// console.log(x.getColorElem("blue"))
+
